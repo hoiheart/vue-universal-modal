90ad8a3bafe9a03000f690221fdf1946e398fea3
hoiheart <hoiheart@gmail.com>
2020-11-08 07:49:21 +0900

    test: add plugin install

---
+++ src/__tests__/index.spec.ts
@@ -0,0 +1,20 @@
+import { mount } from '@vue/test-utils'
+import VueUniversalModal from '@/index'
+
+const TeleportComponent = {
+  template: `
+    <VueUniversalModal />
+  `
+}
+
+describe('VueUniversalModal', () => {
+  it('Mount teleport DOM', () => {
+    // todo: support plugin options test (cannot find use case yet at VTU@2)
+    const wrapper = mount(TeleportComponent, {
+      global: {
+        plugins: [VueUniversalModal]
+      }
+    })
+    expect(wrapper.find('#modals')).toBeTruthy()
+  })
+})
