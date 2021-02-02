8629822123414ed0c56898b00670eda05129bf27
hoiheart <hoiheart@gmail.com>
2021-02-02 09:17:38 +0900

    chore: examples style

---
+++ example/style.ts
@@ -0,0 +1,9 @@
+const h3 = 'mt-6 mb-2 text-xl font-bold'
+const ul = 'mt-2 list-disc list-inside'
+const button = 'mt-2 rounded-full py-1 px-4 bg-white transition border-blue-500 border-2 text-blue-500 text-sm hover:border-blue-700 hover:text-blue-700 focus:bg-blue-700 focus:text-white focus:outline-none'
+
+export const style = {
+  h3,
+  ul,
+  button
+}
